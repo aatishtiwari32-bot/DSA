891183996b8a32b36b5d68996d9402ed25a666f9
Aatish Tiwari <aatishtiwari32@gmail.com>
2026-09-16 23:15:50 +0530

    feat: add LeetCode 20. Valid Parentheses solution (cpp)

---
+++ problems/valid-parentheses/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char ch : s) {
+            // Opening brackets
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            // Closing brackets
+            else {
+                if(st.empty())
+                    return false;
+                char top = st.top();
+                st.pop();
+                if(ch == ')' && top != '(')
+                    return false;
+                if(ch == '}' && top != '{')
+                   return false;
+                if(ch == ']' && top != '[')
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
